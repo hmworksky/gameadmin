e4c90b9745c9aa3a6f9dee9644f592cbc08f4f08
unknown <huangmin@wanlitong.com>
2019-02-14 10:38:17 +0800

    2019-02-14/settings-default

--- HttpRunnerManager/settings.py
+++ HttpRunnerManager/settings.py
@@ -118,9 +118,12 @@ if DEBUG:
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'HttpRunner',  # 新建数据库名
-            'USER': 'root',  # 数据库登录名
-            'PASSWORD': 'test1324',  # 数据库登录密码
-            'HOST': '127.0.0.1',  # 数据库所在服务器ip地址
+            # 'USER': 'root',  # 数据库登录名
+            # 'PASSWORD': 'test1324',  # 数据库登录密码
+            # 'HOST': '127.0.0.1',  # 数据库所在服务器ip地址
+            'USER': 'game_dev',
+            'PASSWORD': 'game_dev123',
+            'HOST': '21.58.201.44',
             'PORT': '3306',  # 监听端口 默认3306即可
         }
     }
