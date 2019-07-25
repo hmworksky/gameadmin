63205064be64534c3930318cb4db951b297bde88
hmworksky <hm_worksky@163.com>
2019-07-25 17:04:15 +0800

    gc

--- mygame/db.py
+++ mygame/db.py
@@ -17,3 +17,20 @@ def registar(dbname):
         }
     }
     return data
+
+
+def auto(dbname):
+    data = {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_game_{}'.format(dbname) if not WIN_FLAG else 'auto_{}'.format(dbname),
+        'USER': 'game_dev' if not WIN_FLAG else 'root',
+        'PASSWORD': 'game_dev123' if not WIN_FLAG else 'test1324',
+        'HOST': '21.58.201.44' if not WIN_FLAG else '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8',
+        }
+    }
+    return data
+
--- mygame/settings.py
+++ mygame/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import sys
-from mygame.db import registar
+from mygame.db import registar, auto
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'mygame.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': registar('default')
+    'default': auto('admin')
 }
 
 DATABASE_ROUTERS = ['mygame.database_router.DatabaseAppsRouter']
